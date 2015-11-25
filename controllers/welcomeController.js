f7df0e9bcc68312175bb5edcc034ac4b82e78515
shinesoleil <soleilpan@gmail.com>
2015-11-25 12:15:44 +0800

    changed some contents

--- controllers/welcomeController.js
+++ controllers/welcomeController.js
@@ -2,7 +2,7 @@ app.controller('welcomeController', [function() {
     this.todoList = [
         {"number": 1, "task": "write articles (of course....)"},
         {"number": 2, "task": "search box"},
-        {"number": 3, "task": "contact page"},
+        {"number": 3, "task": "develop backend with NodeJS and MongoDB"},
         {"number": 4, "task": "test more angular features"},
         {"number": 5, "task": "......"}
     ];
